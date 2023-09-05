3bf186040226a710f01462c9ba894443f09505fa
microvac23 <Ithanmachuca@gmail.com>
2023-09-05 17:25:41 -0500

    thoughts to post conversion

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pet-posts');
 
 module.exports = mongoose.connection;
